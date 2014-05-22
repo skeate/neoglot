05c08deae124790b1bdc6b45ca95b4462c8409a1
Jonathan Skeate <skeate@gmail.com>
2014-05-22 19:48:55 -0400

    ignore production.js (to avoid commiting passwords)

--- config/environments/index.js
+++ config/environments/index.js
@@ -1,4 +1,6 @@
 module.exports = function (app, passport) {
-    require('./development')(app, passport);
-    require('./production')(app, passport);
+	if( app.settings.env == "production" )
+		require('./production')(app, passport);
+	else
+		require('./development')(app, passport);
 };
